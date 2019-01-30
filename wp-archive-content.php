15abfe57e4a651edee379f0ee97a824842d33507
swipe72 <accounts@swipe72.co.uk>
2019-01-30 12:26:34 +0000

    wpac_maybe_process_form

--- wp-archive-content.php
+++ wp-archive-content.php
@@ -37,8 +37,28 @@ function wpac_get_archive_title($title) {
     $post_type_object = get_post_type_object($post_type);
     // Get the option, but return the post_type label if it's not set
     $title = get_option("wpac_{$post_type}_archive_title", $post_type_object->label);
-}
+  }
   // Return what we've got
   return add_filter('wpac_title', $title);
 }
 add_filter('get_the_archive_title', 'wpac_get_archive_title');
+
+/**
+ * Maybe save the archive form
+ * @return void
+ */
+function wpac_maybe_process_form() {
+  // Check for the hidden input
+  if(!isset($_POST['wpac_save_form'])) {
+    return;
+  }
+  // Get the current post_type
+  $post_type = get_post_type();
+  // Check for the title and description
+  $title = isset($_POST['_archive_title']) ? $_POST['_archive_title'] : get_the_archive_title();
+  $description = isset($_POST['_archive_description']) ? $_POST['_archive_description'] : '';
+  // Update the options
+  update_option("wpac_{$post_type}_archive_title", $title);
+  update_option("wpac_{$post_type}_archive_description", $description);
+}
+add_action('admin_init', 'wpac_maybe_process_form');
