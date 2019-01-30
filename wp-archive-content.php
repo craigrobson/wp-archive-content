cc55e8184eb79baa477f73b901f2a7544b8446d9
swipe72 <accounts@swipe72.co.uk>
2019-01-30 12:26:17 +0000

    get_the_archive_title

--- wp-archive-content.php
+++ wp-archive-content.php
@@ -24,3 +24,21 @@ function wpac_get_archive_description($description) {
   return add_filter('wpac_description', $description);
 }
 add_filter('get_the_archive_description', 'wpac_get_archive_description');
+
+/**
+ * Make "the_archive_title" function return our title
+ * Use the "get_the_archive_title" filter to get the custom title
+ * @param string $title
+ * @return string
+ */
+function wpac_get_archive_title($title) {
+  if(is_post_type_archive()) {
+    $post_type = get_post_type();
+    $post_type_object = get_post_type_object($post_type);
+    // Get the option, but return the post_type label if it's not set
+    $title = get_option("wpac_{$post_type}_archive_title", $post_type_object->label);
+}
+  // Return what we've got
+  return add_filter('wpac_title', $title);
+}
+add_filter('get_the_archive_title', 'wpac_get_archive_title');
