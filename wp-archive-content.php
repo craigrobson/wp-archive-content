352260c56fb5801a4a5447e566cc3109234f350f
swipe72 <accounts@swipe72.co.uk>
2019-01-31 10:39:44 +0000

    Class based

--- wp-archive-content.php
+++ wp-archive-content.php
@@ -7,86 +7,97 @@
  * Author: Craig Robson
  */
 
-/**
- * Make "the_archive_description" function return our description
- * Use the "get_the_archive_description" filter to get the custom description
- * @param string $description
- * @return string
- */
-function wpac_get_archive_description($description) {
-  if(is_post_type_archive()) {
-    $post_type = get_post_type();
-    $post_type_object = get_post_type_object($post_type);
-    // Get the option, but return the current description if it's set
-    $description = get_option("wpac_{$post_type}_archive_description", $post_type_object->description);
-  }
-  // Return what we've got
-  return add_filter('wpac_description', $description);
-}
-add_filter('get_the_archive_description', 'wpac_get_archive_description');
+define('WPAC_URL', plugin_dir_url(__FILE__));
 
-/**
- * Make "the_archive_title" function return our title
- * Use the "get_the_archive_title" filter to get the custom title
- * @param string $title
- * @return string
- */
-function wpac_get_archive_title($title) {
-  if(is_post_type_archive()) {
-    $post_type = get_post_type();
-    $post_type_object = get_post_type_object($post_type);
-    // Get the option, but return the post_type label if it's not set
-    $title = get_option("wpac_{$post_type}_archive_title", $post_type_object->label);
-  }
-  // Return what we've got
-  return add_filter('wpac_title', $title);
-}
-add_filter('get_the_archive_title', 'wpac_get_archive_title');
+if(class_exists('WPAC')) {
 
-/**
- * Maybe save the archive form
- * @return void
- */
-function wpac_maybe_process_form() {
-  // Check for the hidden input
-  if(!isset($_POST['wpac_save_form'])) {
-    return;
-  }
-  // Get the current post_type
-  $post_type = get_post_type();
-  // Check for the title and description
-  $title = isset($_POST['_archive_title']) ? $_POST['_archive_title'] : get_the_archive_title();
-  $description = isset($_POST['_archive_description']) ? $_POST['_archive_description'] : '';
-  // Update the options
-  update_option("wpac_{$post_type}_archive_title", $title);
-  update_option("wpac_{$post_type}_archive_description", $description);
-}
-add_action('admin_init', 'wpac_maybe_process_form');
+  class WPAC {
+    /**
+     * Holds the current post_type
+     * @var string
+     */
+    private $post_type = '';
 
-/**
- * Add the form to the post_types edit.php
- * @return void
- */
-function wpac_add_post_type_form() {
-  global $pagenow, $wp;
-  // Stop if we're not on the right page
-  // Or if the post_type query_var is not set
-  if($pagenow !== "edit.php" || !isset($wp->query_vars['post_type'])) {
-    return;
-  }
+    /**
+     * Class constructor
+     * Setup the actions and filters
+     * @return void
+     */
+    public function __construct() {
+      // Enqueue the CSS and JS for the admin
+      add_action('admin_enqueue_scripts', array($this, 'enqueue_assets'));
+      // Add the form to edit.php
+      add_action('admin_init', array($this, 'render_form'));
+    }
 
-  $post_type = $wp->query_vars['post_type'];
-  // If this post_type doesn't "has_archive"
-  $post_type_object = get_post_type_object($post_type);
-  var_dump($post_type_object);
-  ob_start();
+    /**
+     * Enqueue the CSS and JS files
+     * @return void
+     */
+    public function enqueue_assets() {
+      // Stop if we're not on the right page
+      if(!$this->wpac_page()) {
+        return;
+      }
+
+      wp_enqueue_style('wpac-style', WPAC_URL . "/wpac.css");
+      wp_enqueue_script('wpac-scripts', WPAC_URL . "/wpac.js", array('jquery'), null, true);
+    }
+
+    /**
+     * Render the form to edit the content
+     * @return void
+     */
+    public function render_form() {
+      // Stop if we're not on the right page
+      if(!$this->wpac_page()) {
+        return;
+      }
+
+      ob_start();
 ?>
-<div id="">
-
+<div id="wpac-wrapper">
+  <button class="button button-primary" type="button">
+    Edit Archive Content
+  </button>
+  <div id="wpac-editor">
+    <div id="wpac-title-holder">
+      <input type="text" name="title" value="<?php the_archive_title(); ?>" />
+    </div>
+    <div id="wpac-wysiwyg-holder">
+      <?php wp_editor(the_archive_description(), 'description'); ?>
+    </div>
+    <button type="submit" class="button">Update Content</button>
+    <input type="hidden" name="wpac" value="<?php echo $this->post_type; ?>" />
+  </div>
 </div>
 <?php
-  echo ob_get_clean();
+      echo ob_get_clean();
 
+
+    }
+
+    /**
+     * Check if the current page is a WPAC page
+     * @return bool
+     */
+    private function wpac_page() {
+      global $pagenow, $wp;
+      if($pagenow !== "edit.php" || !isset($wp->query_vars['post_type'])) {
+        return false;
+      }
+
+      $this->post_type = $wp->query_vars['post_type'];
+      $post_type_object = get_post_type_object($this->post_type);
+      if(!$post_type_object) {
+        return false;
+      }
+      // If this post_type doesn't "has_archive"
+      return $post_type_object->has_archive;
+    }
+  }
+
+  global $wpac;
+  $wpac = new WPAC;
 
 }
-add_action('admin_notices', 'wpac_add_post_type_form');
